013bb2815265ae79f0d9235bfcbc217ba6f1398b
Jon Cooper <joncoop@users.noreply.github.com>
2023-05-17 11:43:07 -0400

    Add files via upload

---
+++ java/Array-2.java
@@ -0,0 +1,539 @@
+public int countEvens(int[] nums) {
+  int count = 0;
+  
+  for (int n: nums) {
+    if (n % 2 == 0) {
+      count += 1;
+    }
+  }
+  
+  return count;
+}
+  
+
+public int bigDiff(int[] nums) {
+  int max = nums[0];
+  int min = nums[0];
+  
+  for (int n : nums) {
+    max = Math.max(n, max);
+    min = Math.min(n, min);
+  }
+  
+  return max - min;
+}
+  
+
+public int centeredAverage(int[] nums) {
+  int max = nums[0];
+  int min = nums[0];
+  int sum = 0;
+  
+  for (int n : nums) {
+    max = Math.max(n, max);
+    min = Math.min(n, min);
+    sum += n;
+  }
+  
+  return (sum - max - min) / (nums.length - 2);
+}
+  
+
+public int sum13(int[] nums) {
+  int sum = 0;
+  int i = 0;
+  
+  while (i < nums.length) {
+    if (nums[i] != 13) {
+      sum += nums[i];
+      i += 1;  
+    }  
+    else {
+      i += 2;
+    }
+  }
+  
+  return sum;
+}
+  
+
+public int sum67(int[] nums) {
+  boolean countIt = true;
+  int sum = 0;
+  
+  for (int n : nums)
+  {
+    if (n == 6) {
+      countIt = false;
+    }
+  
+    if (countIt) {
+      sum += n;
+    }
+    
+    if (n == 7) {
+      countIt = true;
+    }
+  }
+  
+  return sum;
+}
+
+
+public boolean has22(int[] nums) {
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == 2 && nums[i + 1] == 2) {
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+
+public boolean lucky13(int[] nums) {
+  for (int n : nums) {
+    if (n == 1 || n == 3) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+  
+
+public boolean sum28(int[] nums) {
+  int sum = 0;
+  
+  for (int n : nums) {
+    if (n == 2) {
+      sum += n;
+    }
+  }
+  
+  return sum == 8;
+}
+  
+
+public boolean more14(int[] nums) {
+  int ones = 0;
+  int fours = 0;
+  
+  for (int n: nums) {
+    if (n == 1) {
+      ones++;
+    }
+    else if (n == 4) {
+      fours++;
+    }
+  }
+  
+  return ones > fours;
+}
+  
+
+public int[] fizzArray(int n) {
+  int[] nums = new int[n];
+  
+  for (int i= 0; i < nums.length; i++) {
+    nums[i] = i;
+  }
+    
+  return nums;
+}
+  
+  
+public boolean only14(int[] nums) {
+  for (int n: nums) {
+    if (n != 1 && n != 4) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+  
+
+public String[] fizzArray2(int n) {
+  String[] result = new String[n];
+  
+  for (int i = 0; i < n; i++) {
+    result[i] = Integer.toString(i);
+  }
+  
+  return result;
+}
+  
+
+public boolean no14(int[] nums) {
+  boolean hasOnes = false;
+  boolean hasFours = false;
+  
+  for (int n: nums) {
+    hasOnes = hasOnes || n == 1;
+    hasFours = hasFours || n == 4;
+  }
+  
+  return !hasOnes || !hasFours;
+}
+
+
+public boolean isEverywhere(int[] nums, int val) {
+  int limit = nums.length - 1;
+  
+  for (int i = 0; i < limit; i++) {
+    int n1 = nums[i];
+    int n2 = nums[i + 1];
+    
+    if (n1 != val && n2 != val) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+  
+  
+public boolean either24(int[] nums) {
+  int limit = nums.length -1;
+  boolean hasTwoPair = false;
+  boolean hasFourPair = false;
+  
+  for (int i = 0; i < limit; i++) {
+    int n1 = nums[i];
+    int n2 = nums[i+1];
+    
+    if (n1 == 2 && n2 == 2) {
+      hasTwoPair = true;
+    }
+  
+    if (n1 == 4 && n2 == 4) {
+      hasFourPair = true;
+    }
+  }
+  
+  return hasTwoPair != hasFourPair;
+}
+  
+
+public int matchUp(int[] nums1, int[] nums2) {
+  int count = 0;
+  
+  for (int i = 0; i < nums1.length; i++) {
+    int diff = Math.abs(nums1[i] - nums2[i]);
+    
+    if (diff == 1 || diff == 2) {
+      count++;
+    }
+  }
+  
+  return count;
+}
+  
+
+public boolean has77(int[] nums) {
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == 7) {
+      if (nums[i + 1] == 7){
+        return true;
+      }
+      else if (i < nums.length - 2 && nums[i + 2] == 7) {
+        return true;
+      }
+    }
+  }
+  
+  return false;
+}
+  
+
+public boolean has12(int[] nums) {
+  boolean hasOne = false;
+  
+  for (int n: nums) {
+    if (n == 1) {
+      hasOne = true;
+    }
+    else if (hasOne && n == 2) {
+      return true;
+    }
+  }
+  
+  return false;
+}
+  
+
+public boolean modThree(int[] nums) {
+  for (int i = 0; i < nums.length - 2; i++) {
+    int r1 = nums[i] % 2;
+    int r2 = nums[i + 1] % 2;
+    int r3 = nums[i + 2] % 2;
+  
+    if (r1 == r2 && r2 == r3) {
+      return true;
+    }
+  }
+  
+  return false;
+}
+  
+
+public boolean haveThree(int[] nums) {
+  int count = 0;
+  
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 3) {
+      count++;
+    
+      if (i < nums.length - 1 && nums[i + 1] == 3) {
+        return false;
+      }
+    }
+  }
+  
+  return count == 3;
+}
+
+
+public boolean twoTwo(int[] nums) {
+  boolean twoBefore, twoAfter;
+  
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      twoBefore = i > 0 && nums[i - 1] == 2;
+      twoAfter = i < nums.length - 1 && nums[i + 1] == 2;
+    
+      if (!twoBefore && !twoAfter) {
+        return false;
+      }
+    }
+  }
+  
+  return true;
+}
+  
+
+public boolean sameEnds(int[] nums, int len) {
+  for (int i = 0; i < len; i++) {
+    int j = nums.length - len + i;
+    
+    if (nums[i] != nums[j]) {
+      return false;
+    }  
+  }
+  
+  return true;
+}
+  
+
+
+public boolean tripleUp(int[] nums) {
+  for (int i = 0; i < nums.length - 2; i++) {
+    int a = nums[i];
+    int b = nums[i + 1] - 1;
+    int c = nums[i + 2] - 2;
+    
+    if (a == b && b == c) {
+      return true;
+    }
+  }
+  
+  return false;
+}
+  
+
+public int[] fizzArray3(int start, int end) {
+  int[] result = new int[end - start];
+  
+  for (int i = 0; i < result.length; i++) {
+    result[i] = start + i;
+  }
+  
+  return result;
+}
+  
+
+public int[] shiftLeft(int[] nums) {
+  if (nums.length == 0) {
+    return nums;
+  }
+  
+  int temp = nums[0];
+  
+  for (int i = 1; i < nums.length; i++) {
+    nums[i-1] = nums[i];
+  }
+  
+  nums[nums.length - 1] = temp;
+  
+  return nums;
+}
+  
+
+public int[] tenRun(int[] nums) {
+  boolean streakActive = false;
+  int currentMultiple = 0;
+  
+  for (int i=0; i<nums.length; i++) {
+    if (nums[i] % 10 == 0) {
+      streakActive = true;
+      currentMultiple = nums[i];
+    }
+    else if (streakActive) {
+      nums[i] = currentMultiple; 
+    }
+  }
+  
+  return nums;
+}
+  
+
+public int[] pre4(int[] nums) {
+  int count = 0;
+  
+  while (nums[count] != 4) {
+    count++;
+  }
+  
+  int[] result = new int[count];
+  
+  for (int i = 0; i < result.length; i++) {
+    result[i] = nums[i];
+  }
+  
+  return result;
+}
+  
+
+public int[] post4(int[] nums) {
+  int end = nums.length - 1;
+  
+  while (nums[end] != 4) {
+    end--;
+  }
+  
+  int[] result = new int[nums.length - end - 1];
+  
+  for (int i = 0; i < result.length; i++) {
+    result[i] = nums[i + end + 1];
+  }
+  
+  return result;
+}
+
+
+public int[] notAlone(int[] nums, int val) {
+  for (int i = 1; i < nums.length - 1; i++) {
+    int before = nums[i - 1];
+    int current = nums[i];
+    int after = nums[i + 1];
+    
+    if (current == val && current != before && current != after) {
+      nums[i] = Math.max(before, after);
+    }
+  }
+  
+  return nums;
+}
+  
+
+public int[] zeroFront(int[] nums) {
+  int[] result = new int[nums.length];
+  int loc = nums.length - 1;
+  
+  for (int n: nums) {
+    if (n != 0) {
+      result[loc] = n;
+      loc--;
+    }
+  }
+  
+  return result;
+}
+  
+
+public int[] withoutTen(int[] nums) {
+  int[] result = new int[nums.length];
+  
+  int insert = 0;
+  
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] != 10) {
+      result[insert] = nums[i];
+      insert++;
+    }
+  }
+  
+  return result;
+}
+  
+
+public int[] zeroMax(int[] nums) 
+{
+  int maxOdd = 0;
+  
+  for (int i=nums.length-1; i>=0; i--)
+  {
+    if (nums[i] % 2 == 1 && nums[i] > maxOdd)
+    {
+      maxOdd = nums[i];
+    }
+  
+    if (nums[i] == 0)
+    {
+      nums[i] = maxOdd;
+    }
+  }
+  
+  return nums;
+}
+
+
+public int[] evenOdd(int[] nums) {
+  int[] result = new int[nums.length];
+  int evenLoc = 0;
+  int oddLoc = nums.length - 1;
+  
+  for (int n: nums) {
+    if (n % 2 == 0) {
+      result[evenLoc] = n;
+      evenLoc++;
+    }
+    else {
+      result[oddLoc] = n;
+      oddLoc--;
+    }
+  }
+  
+  return result;
+}
+
+
+// helper function for fizzBuzz
+public String getValue(int n) {
+  String result = "";
+  
+  if (n % 3 == 0) {
+    result += "Fizz";
+  }
+  
+  if (n % 5 == 0) {
+    result += "Buzz";
+  }
+  
+  if (result.length() == 0) {
+    result += String.valueOf(n);
+  }
+  
+  return result;
+}
+  
+public String[] fizzBuzz(int start, int end) {
+  String[] result = new String[end - start];
+  
+  for (int i = 0; i < result.length; i++) {
+    result[i] = getValue(i + start);
+  }
+  
+  return result;
+}
---
+++ java/Logic-1.java
@@ -0,0 +1,372 @@
+public boolean cigarParty(int cigars, boolean isWeekend) {
+  if (isWeekend) {
+    return cigars >= 40;
+  }
+  else {
+    return cigars >= 40 && cigars <= 60;
+  }
+}
+  
+
+public int dateFashion(int you, int date) {
+  if (you <= 2 || date <= 2) {
+    return 0;
+  }
+  else if (you >= 8 || date >= 8) {
+    return 2;
+  }
+  else {
+    return 1;
+  }
+}
+  
+
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  int lower = 60;
+  int upper = 90;
+  
+  if (isSummer) {
+    upper = 100;
+  }
+  
+  return lower <= temp && temp <= upper;
+}
+
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  if (isBirthday) {
+    speed -= 5;
+  }
+  
+  if (speed <= 60) {
+    return 0;
+  }
+  else if (speed > 60 && speed <= 80) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}
+  
+
+public int sortaSum(int a, int b) {
+  int sum = a + b;
+  
+  if (10 <= sum && sum <= 20) {
+    sum = 20;
+  }
+  
+  return sum;
+}
+  
+  
+public String alarmClock(int day, boolean vacation) {
+  String early = "7:00";
+  String late = "10:00";
+  
+  if (vacation) {
+    early = "10:00";
+    late = "off";
+  }
+  
+  if (day >= 1 && day <= 5) {
+    return early;
+  }
+  else {
+    return late;
+  }
+}
+  
+
+public boolean love6(int a, int b) {
+  int sum = a + b;
+  int diff = Math.abs(a - b);
+  
+  return a == 6 || b == 6 || sum == 6 || diff == 6;
+}
+  
+
+public boolean in1To10(int n, boolean outsideMode) {
+  
+  if (outsideMode) {
+    return n <= 1 || n >= 10;
+  }
+  else {
+    return n >= 1 && n <= 10;
+  }
+}
+  
+
+public boolean specialEleven(int n) {
+  return n % 11 <= 1;
+}
+  
+
+public boolean more20(int n) {
+  int diff = n % 20;
+  
+  return diff == 1 || diff == 2;
+}
+  
+
+public boolean old35(int n) {
+  boolean mult3 = n % 3 == 0;
+  boolean mult5 = n % 5 == 0;
+  boolean mult15 = mult3 && mult5;
+  
+  return !mult15 && (mult3 || mult5);
+}
+ 
+
+public boolean less20(int n) {
+  int diff = n % 20;
+  
+  return diff == 18 || diff == 19;
+}
+  
+
+public boolean nearTen(int num) {
+  int ones = num % 10;
+  
+  return ones <= 2 || ones >= 8;
+}
+  
+
+public int teenSum(int a, int b) {
+  boolean aTeen = a >= 13 && a <= 19;
+  boolean bTeen = b >= 13 && b <= 19;
+  
+  if (aTeen || bTeen) {
+    return 19;
+  }
+  else {
+    return a + b;
+  }
+}
+
+
+public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+  if (isAsleep) {
+    return false;
+  }
+  
+  return isMom || !isMorning;
+}
+
+
+public int teaParty(int tea, int candy) {
+  boolean notEnoughOfSomething = tea < 5 || candy < 5;
+  boolean twiceAsMuchTea = tea >= 2 * candy;
+  boolean twiceAsMuchCandy = candy >= 2 * tea;
+  
+  if (notEnoughOfSomething) {
+    return 0;
+  }
+  else if (twiceAsMuchTea || twiceAsMuchCandy) {
+    return 2;
+  }
+  else {
+    return 1;
+  }
+}
+
+
+public String fizzString(String str) {
+  boolean startsWithF = str.charAt(0) == 'f';
+  boolean endsWithB = str.charAt(str.length() - 1) == 'b';
+  
+  if (startsWithF && endsWithB) {
+    return "FizzBuzz";
+  }
+  else if (startsWithF) {
+    return "Fizz";
+  }
+  else if (endsWithB) {
+    return "Buzz";
+  }
+  else {
+    return str;
+  }
+}
+  
+
+public String fizzString2(int n) {
+  String result = "";
+  
+  if (n % 15 == 0) {
+    result = "FizzBuzz";
+  }
+  else if (n % 3 == 0) {
+    result = "Fizz";
+  }
+  else if (n % 5 == 0) {
+    result = "Buzz";
+  }
+  else {
+    result = Integer.toString(n);
+  }
+  
+  return result + "!";
+}
+  
+
+public boolean twoAsOne(int a, int b, int c) {
+
+  int a_plus_b = a + b;
+  int b_plus_c = b + c;
+  int a_plus_c = a + c;
+  
+  return a_plus_b == c || b_plus_c == a || a_plus_c == b;
+}
+  
+  
+public boolean inOrder(int a, int b, int c, boolean bOk) {
+  if (bOk) {
+    return b < c;
+  }
+  else {
+    return a < b && b < c;
+  }
+}
+  
+
+public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+  // a clear solution
+  /*
+  if (equalOk) {
+    return a <= b && b <= c;
+  }
+  else {
+    return a < b && b < c;
+  }
+  */
+  
+  // a slightly too 'clever' solution, maybe
+  if (equalOk) {
+    b += 1; c += 2;
+  }
+  
+  return a < b && b < c;
+}
+  
+
+public boolean lastDigit(int a, int b, int c) {
+  int aLast = a % 10;
+  int bLast = b % 10;
+  int cLast = c % 10;
+  
+  return aLast == bLast || bLast == cLast || aLast == cLast;
+}
+  
+
+public boolean lessBy10(int a, int b, int c) {
+  int abDiff = Math.abs(a - b);
+  int acDiff = Math.abs(a - c);
+  int bcDiff = Math.abs(b - c);
+  
+  return abDiff >= 10 || acDiff >= 10 || bcDiff >= 10;
+}
+
+
+public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+  if (noDoubles && die1 == die2) {
+    die1 += 1;
+    
+    if (die1 > 6) {
+      die1 = 1;
+    }
+  }
+  
+  return die1 + die2;
+}
+  
+  
+public int maxMod5(int a, int b) {
+  if (a == b) {
+    return 0;
+  }
+  
+  int aRem = a % 5;
+  int bRem = b % 5;
+  
+  if (aRem == bRem) {
+    return Math.min(a, b);
+  }
+  else {
+    return Math.max(a, b);
+  }
+}
+
+
+public int redTicket(int a, int b, int c) {
+  if (a == 2 && b == 2 && c == 2) {
+    return 10;
+  }
+  else if (a == b && b == c) {
+    return 5;
+  }
+  else if (a != b && a != c) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+  
+
+public int greenTicket(int a, int b, int c) {
+  if (a == b && b == c) {
+    return 20;
+  }
+  else if (a == b || b == c || a == c) {
+    return 10;
+  }
+  else {
+    return 0;
+  }
+}
+  
+
+public int blueTicket(int a, int b, int c) {
+  int ab = a + b;
+  int bc = b + c;
+  int ac = a + c;
+  
+  if (ab == 10 || bc == 10 || ac == 10) {
+    return 10;
+  }
+  else if (ab == bc + 10 || ab == ac + 10) {
+    return 5;
+  }
+  else {
+    return 0;
+  }
+}
+
+
+public boolean shareDigit(int a, int b) {
+  int aOnes = a % 10;
+  int aTens = a / 10;
+  int bOnes = b % 10;
+  int bTens = b / 10;
+  
+  return aOnes == bOnes || aOnes == bTens || 
+         aTens == bOnes || aTens == bTens;
+}
+  
+  
+public int sumLimit(int a, int b) {
+  int sum = a + b;
+  
+  int aDigits = (String.valueOf(a)).length();
+  int sumDigits = String.valueOf(sum).length();
+  
+  if (sumDigits > aDigits) {
+    return a;
+  }
+  else {
+    return sum;
+  }
+}
+  
