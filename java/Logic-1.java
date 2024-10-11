6f7a6a9871d390d19be2fbd2894cd31fb0c4cea6
Jon Cooper <joncoop@users.noreply.github.com>
2024-10-11 11:04:25 -0400

    Update Logic-1.java

--- java/Logic-1.java
+++ java/Logic-1.java
@@ -355,7 +355,7 @@ public boolean shareDigit(int a, int b) {
 public int sumLimit(int a, int b) {
   int sum = a + b;
   
-  int aDigits = (String.valueOf(a)).length();
+  int aDigits = String.valueOf(a).length();
   int sumDigits = String.valueOf(sum).length();
   
   if (sumDigits > aDigits) {
